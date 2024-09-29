ec91853964b8e27a2cb2d32df1dd1ab019e70242
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-30 00:33:18 +0600

    update: second navbar structure

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,11 +1,22 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar:React.FC = () => {
     return (
         <nav className=''>
-            <div className="navbar bg-base-100">
+            <Navbar1/>
+            <Navbar2/>
+        </nav>
+    );
+};
+
+export default Navbar;
+
+
+const Navbar1:React.FC = ()=>{
+  return  <section className="navbar bg-base-100">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="text-xl">rentwheels@gmail.com</a>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
@@ -21,61 +32,63 @@ const Navbar:React.FC = () => {
       </li>
     </ul>
   </div>
-</div>
-            <section className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
+</section>
+}
+
+
+const Navbar2:React.FC = ()=>{
+    return  <section className="navbar bg-base-100">
+    <div className="navbar-start">
+      <div className="dropdown">
+        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16" />
+          </svg>
+        </div>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <li><Link href='/'>Home</Link></li>
+          <li>
+            <a>Parent</a>
+            <ul className="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li>
+          <li><a>Item 3</a></li>
+        </ul>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
+      <a className="btn btn-ghost text-xl">Rent Wheels</a>
+    </div>
+    <div className="navbar-center hidden lg:flex">
+      <ul className="menu menu-horizontal px-1">
+      <li><Link href='/'>Home</Link></li>
         <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
+          <details>
+            <summary>Services</summary>
+            <ul className="p-2">
+              <li><Link href='/cars'>Cars</Link></li>
+              <li><Link href='/cars'>Trucks</Link></li>
+              <li><Link href='/offers'>Locations</Link></li>
+              <li><Link href='/location'>Special Offers</Link></li>
+            </ul>
+          </details>
         </li>
-        <li><a>Item 3</a></li>
+        <li><a>Career</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</section>
-        </nav>
-    );
-};
-
-export default Navbar;
+    <div className="navbar-end">
+      <button className="btn">Book a vehicle</button>
+    </div>
+  </section>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,6 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme='light'>
       <body
-    
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
         {children}
