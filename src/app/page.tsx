e712b3c9ecdf562a679a530ecc5015c19fd0433b
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-30 16:39:41 +0600

    add: car booking form on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,22 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <main>
-      bg-[url()]  bg-cover
-      <header className='w-full h-[100vh] relative'>
+      <header className='w-full h-[140vh] relative'>
         <div className="absolute inset-0 bg-[url('/images/banner.jpg')] w-full h-full bg-cover"></div>
         <div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent w-full h-full flex justify-center items-center">
         <h1 className='text-6xl font-bold '>Welcome to the</h1>
+        <SearchForm/>
         </div>
-          
       </header>
     </main>
   );
 };
 
 export default App; 
+
+
+const SearchForm:React.FC=()=>{
+      return <section className='w-10/12 mx-auto h-[400px] bg-[#e88409] absolute bottom-[-240px]'>
+
+      </section>
+}
