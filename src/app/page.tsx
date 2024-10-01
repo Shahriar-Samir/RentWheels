36573faceb1ed6790c9b9c1df1dc95090ebc55cf
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-02 04:27:27 +0600

    update: fike

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
       <header className='w-full h-[140vh] relative'>
         <div className="absolute inset-0 bg-[url('/images/banner.jpg')] w-full h-full bg-cover"></div>
         <div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent w-full h-full flex justify-center items-center">
-        <h1 className='text-6xl font-bold '>Welcome to the</h1>
+        <h1 className='text-6xl font-bold '>Welcome to</h1>
         <SearchForm/>
         </div>
       </header>
